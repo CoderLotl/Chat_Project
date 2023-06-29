7fe16a995dc40726969a73156f804537e0a97d89
CoderLotl <j.e.delmar.87@gmail.com>
2023-06-29 11:19:58 -0300

    Can't open db. Path is wrong.

--- lib/services/DataAccess.php
+++ lib/services/DataAccess.php
@@ -4,7 +4,21 @@ class DataAccess
 {
     public function Login($username, $password)
     {
+        $pdo = new PDO('sqlite:' . './database/Database.db');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try
+        {
+            $statement = $pdo->prepare("SELECT COUNT(*) FROM users WHERE name = :name AND password = :password");
+            $statement->bindParam(':name', $username, PDO::PARAM_STR);
+            $statement->bindParam(':password', $password, PDO::PARAM_STR);
+            $statement->execute();
 
+            return $statement->fetchColumn() > 0;
+        }
+        catch(Exception $e)
+        {
+
+        }
     }
 }
 
